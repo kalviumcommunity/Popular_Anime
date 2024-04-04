f0a1b82ab4e6ebf198f9ea1dc8b9d8b06fc85967
kowsikadevi21082006 <deva36rp@gamil.com>
2024-04-04 10:38:04 +0530

    connected to my front-end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/home'
 import About from './components/aboutpage'
 import Apipage from './components/apipage'
 import Mainpage from './components/mainpage'
+import Main1 from './components/main1'
 function App() {
   return (
     <>
@@ -11,7 +12,8 @@ function App() {
         <Route path='/' element={<Home/>}></Route>
         <Route path='/about' element={<About/>}></Route>
         <Route path='/apipage' element={<Apipage/>}></Route>
-        <Route path='/mainpage' element={<Mainpage/>}></Route>
+        <Route path='/mainpage1' element={<Mainpage/>}></Route>
+        <Route path='/mainpage' element={<Main1/>}></Route>
       </Routes>
     </>
   );
--- client/src/components/mainpage.jsx
+++ client/src/components/mainpage.jsx
@@ -1,28 +1,70 @@
 import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import './mainpage.css'
+import './main1'
+import axios from "axios";
+import { useLocation } from 'react-router-dom';
 function Main() {
+  const location = useLocation();
+  const [index, setIndex] = useState(parseInt(localStorage.getItem('pos'), 10) || 0);
+  const [ani, setAni] = useState([]);
+  console.log(index)
+  const increment = () => {
+    setIndex(prevIndex => (prevIndex < 22 ? prevIndex + 1 : 0));
+  };
+  
+  const decrement = () => {
+    setIndex(prevIndex => (prevIndex > 0 ? prevIndex - 1 : 22));
+  };
+  window.addEventListener("keydown", function(event){
+    switch (event.key){
+        case "ArrowRight":
+            increment()
+            break;
+    }
+  });
+   window.addEventListener("keydown", function(event){
+    switch (event.key){
+        case "ArrowLeft":
+            decrement()
+            break;
+    }
+  });
+  useEffect(() => {
+    axios.get('http://localhost:3000/api')
+      .then(response => {
+        console.log(response.data[0]);
+        console.log(response.data)
+        setAni(response.data);
+        
+      })
+      .catch(err => console.log(err));
+  }, []); 
   return (
     <div className='Background'>
+      
       <div>
-        <h2 className='ani-name'>Naruto</h2>
-        <h4 className='ani-authorname'>Masashi Kishimoto</h4>
+        {ani.length > 0 &&<h2 className='ani-name'>{ani[index].anime}</h2>}
+        {ani.length > 0 && <h4 className='ani-authorname'>{ani[index].authorname}</h4>}
       </div>
       <div className='anime-data'>
         <h3 className='start-date'>Start Date: </h3>
-        <h3 className='theme'> Theme:</h3>
-        <h3 className='character'> Character:</h3>
+        <h3 className='theme'>Theme:</h3>
+        <h3 className='character'>Character:</h3>
       </div>
       <div className='anime-datas'>
-        <h5 className='sta-date'>October3, 2002 </h5>
-        <h5 className='the'> Hardships</h5>
-        <h5 className='char'>Naruto Uzumaki</h5>
+        {ani.length > 0 && <h5 className='sta-date'>{ani[index].start_date} </h5>}
+        {ani.length > 0 &&<h5 className='the'>{ani[index].theme}</h5>}
+        {ani.length > 0 && <h5 className='char'>{ani[index].character}</h5>}
       </div>
       <div>
-        <img src="https://i.pinimg.com/736x/22/de/8d/22de8dbda07ba5571a97736b12905a17.jpg" alt="" className='ani-image'/>
+      {ani.length > 0 &&<img src={ani[index].image_url} alt="" className='ani-image'/>}
       </div>
-      <button className='back-btn'>← Back</button>
-      <button className='next-btn'>Next →</button>
-      <nav >
+      <Link to='/mainpage' className='back-button'>Animes List</Link>
+      <button className='back-btn' onClick={decrement}>Back</button>
+      <button className='next-btn' onClick={increment}>Next</button>
+
+      <nav className='all'>
         <Link to='/' className='link'>Home</Link>
         <Link to='/about' className='link'>About</Link>
         <Link to='/mainpage' className='link'>Explore</Link>
