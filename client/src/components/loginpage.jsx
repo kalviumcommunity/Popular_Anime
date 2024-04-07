0db91e533c0b567aa2136e6c49e3d11b6bacb1e0
kowsikadevi21082006 <deva36rp@gamil.com>
2024-04-07 13:20:33 +0530

    adding relation between entites

--- client/src/components/loginpage.jsx
+++ client/src/components/loginpage.jsx
@@ -1,63 +1,3 @@
-// import React, { useState } from "react";
-// import { useNavigate, Link } from 'react-router-dom';
-// import "./signup.css";
-
-// function Login() {
-//     const navigate = useNavigate();
-//     const [userName, setUserName] = useState('');
-//     const [password, setPassword] = useState('');
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-//         try {
-//             const response = await fetch('http://localhost:3000/api/login', {
-//                 method: "POST",
-//                 headers: { "Content-Type": "application/json" },
-//                 body: JSON.stringify({ userName, password })
-//             });
-
-//             if (response.ok) {
-//                 const feedback = await response.json();
-//                 const { accessToken, userName } = feedback;
-//                 const currentDate = new Date();
-//                 const nextYear = new Date(currentDate);
-//                 nextYear.setUTCFullYear(nextYear.getUTCFullYear() + 1);
-//                 const expires = nextYear.toUTCString();
-//                 document.cookie = `accessToken=${accessToken};expires=${expires};path=/;`;
-//                 document.cookie = `user=${userName};expires=${expires};path=/;`;
-//                 console.log("Login successful");
-//                 navigate('/');
-//             } else {
-//                 console.log("Login error");
-//             }
-//         } catch (err) {
-//             console.error(err);
-//         }
-//     }
-
-//     return (
-//         <div className="loginbody">
-//             <form onSubmit={handleSubmit} className="login-field">
-//                 <h2 id="signup">Sign up</h2>
-//                 <p className="para">Sign up to continue to anime website!</p>
-
-//                 <div>
-//                     <input type="text" placeholder="username" id="username" value={userName} onChange={(e) => setUserName(e.target.value)} required />
-//                 </div>
-//                 <div>
-//                     <input type="password" placeholder="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-//                 </div>
-//                 <button type="submit" id="submit">Log in</button>
-//             </form>
-//             <Link to='/' className="backtohome-btn">← Back to home</Link>
-//         </div>
-//     );
-// }
-
-// export default Login;
-
-
-
 import React, { useState } from "react";
 import { useNavigate, Link } from 'react-router-dom';
 import axios from "axios";
@@ -122,5 +62,3 @@ function Login() {
 }
 
 export default Login;
-
-    
